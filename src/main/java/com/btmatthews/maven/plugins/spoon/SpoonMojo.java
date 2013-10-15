57d072d19a3ff12ede0a2d3a938c667f328c8553
Brian Matthews <brian@btmatthews.com>
2013-10-15 09:22:32 +0100

    Parse source to determine Java compliance level

--- src/main/java/com/btmatthews/maven/plugins/spoon/SpoonMojo.java
+++ src/main/java/com/btmatthews/maven/plugins/spoon/SpoonMojo.java
@@ -30,14 +30,20 @@ import spoon.support.QueueProcessingManager;
 import spoon.support.StandardEnvironment;
 
 import java.io.File;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+/**
+ * @author <a href="mailto:brian@btmatthews.com">Brian Matthews</a>
+ * @since 1.0.0
+ */
 @Mojo(name = "spoon", defaultPhase = LifecyclePhase.PROCESS_SOURCES)
 public class SpoonMojo extends AbstractMojo {
 
-    @Parameter(defaultValue = "1.5", required = true)
+    @Parameter(defaultValue = "${maven.compier.source}", required = true)
     private String source;
     @Parameter(required = true)
-    private File[] sources;
+    private File[] inputSources;
     @Parameter(required = true)
     private String[] processors;
     @Parameter(defaultValue = "${project.build.directory}/spooned", required = true)
@@ -45,32 +51,37 @@ public class SpoonMojo extends AbstractMojo {
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        try {
-            final StandardEnvironment env = new StandardEnvironment();
-            env.setComplianceLevel(5);
+        final Matcher matcher = Pattern.compile("^1.([5-8])$").matcher(source);
+        if (matcher.matches()) {
+            try {
+                final StandardEnvironment env = new StandardEnvironment();
+                env.setComplianceLevel(Integer.valueOf(matcher.group(1)));
 
-            final Factory factory = new Factory(new DefaultCoreFactory(), env);
+                final Factory factory = new Factory(new DefaultCoreFactory(), env);
 
-            final Builder builder = factory.getBuilder();
-            for (final File source : sources) {
-                builder.addInputSource(source);
+                final Builder builder = factory.getBuilder();
+                for (final File inputSource : inputSources) {
+                    builder.addInputSource(inputSource);
+                }
+                builder.build();
+
+                final ProcessingManager processing = new QueueProcessingManager(factory);
+                for (String processor : processors) {
+                    processing.addProcessor(processor);
+                }
+                processing.process();
+
+                env.getDefaultFileGenerator().setOutputDirectory(outputDirectory);
+                final ProcessingManager printing = new QueueProcessingManager(factory);
+                printing.addProcessor(env.getDefaultFileGenerator());
+                printing.process();
+
+            } catch (final Exception e) {
+                getLog().error(e.getMessage(), e);
+                throw new MojoExecutionException(e.getMessage(), e);
             }
-            builder.build();
+        } else {
 
-            final ProcessingManager processing = new QueueProcessingManager(factory);
-            for (String processor : processors) {
-                processing.addProcessor(processor);
-            }
-            processing.process();
-
-            env.getDefaultFileGenerator().setOutputDirectory(outputDirectory);
-            final ProcessingManager printing = new QueueProcessingManager(factory);
-            printing.addProcessor(env.getDefaultFileGenerator());
-            printing.process();
-
-        } catch (final Exception e) {
-            getLog().error(e.getMessage(), e);
-            throw new MojoExecutionException(e.getMessage(), e);
         }
     }
 }
